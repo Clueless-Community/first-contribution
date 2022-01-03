e9e4002d3630e8a9330f2df91ccd9e483fdd8a1e
itsmeAnubhab <91974827+itsmeAnubhab@users.noreply.github.com>
2022-01-03 18:40:12 +0530

    Update contributors.js

--- data/contributors.js
+++ data/contributors.js
@@ -26,6 +26,15 @@ const contributors = [
         github : "https://github.com/Rajdip019/Rajdip019",
         gender : "male"
     },
+    {
+        name : "Anubhab Halder",
+        branch : "CSE AI & ML",
+        roll : 2161059,
+        year : 2025,
+        linkedin : "https://www.linkedin.com/in/anubhab-halder-289bb2218?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmvRh%2FglXQOOFl%2BRnTKqLOA%3D%3D",
+        github : "https://github.com/itsmeAnubhab",
+        gender : "male"
+    },
 ]
 
 export default contributors
