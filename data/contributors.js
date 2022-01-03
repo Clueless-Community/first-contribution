916ad288ae6e143347e79a5fcb30105d3912d1ef
Rajdeep <rajdipgupta019@gmail>
2022-01-03 20:11:51 +0530

    ClueLess V-1.0

--- data/contributors.js
+++ data/contributors.js
@@ -13,17 +13,26 @@ const contributors = [
         branch : "CSE AI & ML",
         roll : 2160142,
         year : 2025,
-        linkedin : "https://www.linkedin.com/in/rajdeep-sengupta/",
-        github : "https://github.com/Rajdip019/Rajdip019",
+        linkedin : "https://www.linkedin.com/in/roshan-kumar-176918202/",
+        github : "https://github.com/Roshaen",
         gender : "male"
     },
     {
         name : "Nikhil Raj",
         branch : "CSE AI & ML",
-        roll : 2160142,
+        roll : 2161013,
         year : 2025,
-        linkedin : "https://www.linkedin.com/in/rajdeep-sengupta/",
-        github : "https://github.com/Rajdip019/Rajdip019",
+        linkedin : "",
+        github : "https://github.com/nikhil25803",
+        gender : "male"
+    },
+    {
+        name : "Anubhab Halder",
+        branch : "CSE AI & ML",
+        roll : 2161059,
+        year : 2025,
+        linkedin : "https://www.linkedin.com/in/anubhab-halder-289bb2218/",
+        github : "https://github.com/itsmeAnubhab",
         gender : "male"
     },
 ]
