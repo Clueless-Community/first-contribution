cab64bb24ab6a99091cfc4788f7209dfa5e8817b
Ankush Ladani <ankushladani@gmail.com>
2022-04-09 21:19:11 +0530

    Added My Data

---
+++ contributors.js
@@ -0,0 +1,18 @@
+const obj = {
+    name : "Your Name",
+    branch : "Your stream/branch",
+    college : "College Name",
+    year : 2025, //Year of Pass Out
+    linkedin : "LinkedIn Profile URL",
+    github : "GitHub Profile URL",
+    gender : "Your Gender"
+}
+const obj2 = {
+    name : 'Ankush',
+    branch : 'Instrumentation and Control Engineering',
+    college : 'Dharmsinh Desai University',
+    year : 2024,
+    linkedin : 'https://www.linkedin.com/in/ankush-ladani-6a0988199/',
+    github : 'https://github.com/Ankush-Ladani',
+    gender : 'male'
+}
