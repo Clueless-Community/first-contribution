ec16dfd92fb6dd136fd0a13266f0cf94feb974d4
Rajdeep Sengupta <91758830+Rajdip019@users.noreply.github.com>
2022-09-29 19:25:19 +0530

    Delete contributors.js

--- contributors.js
+++
@@ -1,9 +0,0 @@
-const info = {
-    name : 'Ankush',
-    branch : 'Instrumentation and Control Engineering',
-    college : 'Dharmsinh Desai University',
-    year : 2024,
-    linkedin : 'https://www.linkedin.com/in/ankush-ladani-6a0988199/',
-    github : 'https://github.com/Ankush-Ladani',
-    gender : 'male'
-}
