73edee7b7db73de46a6f278e6c6368f8e8883329
mewxz029 <mewlawbreakertv@gmail.com>
2022-10-01 16:22:26 +0700

    fix: resolve conflict

--- data/contributors.js
+++ data/contributors.js
@@ -362,7 +362,16 @@ const contributors = [
   linkedin: "https://www.linkedin.com/in/yash-gupta-1106/",
   github: "https://github.com/yashg-116",
   gender: "male",
-}
+},
+{
+  name : "Wuttinan Sukpoon",
+  branch : "Business IT Management",
+  college : "Rajamangala University of Technology Rattanakosin",
+  year : 2022,
+  linkedin : "-",
+  github : "https://github.com/mewxz029",
+  gender : "Male"
+},
 ];
 
 export default contributors;
