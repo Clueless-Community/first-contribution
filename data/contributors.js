2cd4676ad8ffb792657aa5e477281e91d46bc8b5
saifltr <bagmarusaifs@gmail.com>
2022-10-02 15:49:56 +0530

    Added my data (Saif Bagmaru

--- data/contributors.js
+++ data/contributors.js
@@ -587,6 +587,14 @@ const contributors = [
     github: "https://github.com/minhaj-313",
     gender: "Male",
 },
+{ 
+   name: "Saif Bagmaru", 
+   branch: "Artificial Intelligence and Data Science", 
+   college: "AISSMS Institute of Information Technology, Pune", 
+   year: 2024, linkedin: "https://in.linkedin.com/in/saif-bagmaru", 
+   github: "https://github.com/saifltr",
+   gender: "Male", 
+},
 ];
 
 export default contributors;
