653c411dfcbc8a2ea41b86caa8fc63118868242a
RidhikaJoshi <ridhikasmrh@gmail.com>
2022-10-02 16:24:09 +0530

    Added Changes
    Signed-off-by: RidhikaJoshi <ridhikasmrh@gmail.com>

--- data/contributors.js
+++ data/contributors.js
@@ -587,6 +587,15 @@ const contributors = [
     github: "https://github.com/minhaj-313",
     gender: "Male",
 },
+{
+  name : "Ridhika Joshi",
+  branch : "Computer Science Engineering",
+  college : "Academy of Technology",
+  year : 2025,
+  linkedin : "https://www.linkedin.com/in/ridhika-joshi-069164221/",
+  github : "https://github.com/RidhikaJoshi",
+  gender : "Female"
+},
 ];
 
 export default contributors;
