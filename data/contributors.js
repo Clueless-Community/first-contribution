3b5128bfc7758b0ac1fbfcb11082af55157a926d
saifltr <bagmarusaifs@gmail.com>
2022-10-02 18:08:44 +0530

    Added my data

--- data/contributors.js
+++ data/contributors.js
@@ -588,12 +588,12 @@ const contributors = [
     gender: "Male",
 },
 { 
-   name: "Saif Bagmaru", 
-   branch: "Artificial Intelligence and Data Science", 
-   college: "AISSMS Institute of Information Technology, Pune", 
-   year: 2024, linkedin: "https://in.linkedin.com/in/saif-bagmaru", 
-   github: "https://github.com/saifltr",
-   gender: "Male", 
+  name: "Saif Bagmaru", 
+  branch: "Artificial Intelligence and Data Science", 
+  college: "AISSMS Institute of Information Technology, Pune", 
+  year: 2024, linkedin: "https://in.linkedin.com/in/saif-bagmaru", 
+  github: "https://github.com/saifltr",
+  gender: "Male", 
 },
 ];
 
