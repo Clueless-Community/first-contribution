1ef7c2e6758ad0184ee4d09d3feb44e119cb5f70
FahimFBA <fahimbinamin@gmail.com>
2022-10-03 12:24:38 +0600

    resolved conflict

--- data/contributors.js
+++ data/contributors.js
@@ -708,22 +708,31 @@ const contributors = [
     gender: "male",
   },
   {
-        name : "Swetha N",
-        branch : "Computer Science and Engineering",
-        college : "SASTRA Univerity",
-        year : 2024, //Year of Pass Out
-        linkedin : "https://www.linkedin.com/in/swetha-n-49ba38212/",
-        github : "https://github.com/SwethaN125",
-        gender : "female"
-    },
-    {
-      name : "Md. Fahim Bin Amin",
-      branch : "Computer Science and Engineering",
-      college : "United International University",
-      year : 2025, //Year of Pass Out
-      linkedin : "https://www.linkedin.com/in/fahimfba/",
-      github : "https://github.com/FahimFBA",
-      gender : "male"
+    name: "Swetha N",
+    branch: "Computer Science and Engineering",
+    college: "SASTRA Univerity",
+    year: 2024, //Year of Pass Out
+    linkedin: "https://www.linkedin.com/in/swetha-n-49ba38212/",
+    github: "https://github.com/SwethaN125",
+    gender: "female",
+  },
+  {
+    name: "Sumit Kumar",
+    branch: "Electrical & Electronics Engineering",
+    college: "IPS Academy Indore",
+    year: 2023,
+    linkedin: "https://www.linkedin.com/in/sumit-kumar-rai-b59290233/",
+    github: "https://github.com/sumitkr2000",
+    gender: "Male",
+  },
+  {
+    name: "Md. Fahim Bin Amin",
+    branch: "Computer Science and Engineering",
+    college: "United International University",
+    year: 2025, //Year of Pass Out
+    linkedin: "https://www.linkedin.com/in/fahimfba/",
+    github: "https://github.com/FahimFBA",
+    gender: "male",
   }
 ];
 
