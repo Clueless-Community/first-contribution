38244b12ad86f79b201b6d9956ac1821da39487b
akshay-Kalekar <akalekar2003@gmail.com>
2022-10-05 03:07:36 +0530

    Added a scroll down suggestor

--- pages/components/HeroLight.js
+++ pages/components/HeroLight.js
@@ -95,7 +95,16 @@ const HeroLight = () => {
       <button className="px-4 py-2 border-[#0061ff] border-4 text-[#0061ff] rounded-xl font-bold text-xl my-10 mx-5 hover:bg-blue-500 hover:text-white transition-all hover:border-blue-500">Contribute Now</button>
       </a>
       </div>
+      <div class="flex justify-center pt-[20vh]">
+    <div class="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+      <svg class="w-6 h-6 textblack" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+      </svg>
       </div>
+      </div>
+      <h1 className=" animate-bounce pt-[5vh] text-center text-5xw font-sans font-bold text-black opacity-90">Scroll Down</h1>
+      </div>
+      
       
     </div>
   );
