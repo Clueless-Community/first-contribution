4e9b8d5ba974100735febec7d7913071474c2b06
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-10-04 08:38:25 +0200

    add image to footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -10,20 +10,7 @@ const Footer = () => {
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 dark:text-gray-50">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-7 w-7"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                />
-              </svg>
+              <img className="w-6" src="/clueless_logo.png" alt="clueless logo" />
               <span className="ml-3 text-xl">ClueLess</span>
             </a>
             <p className="mt-2 text-sm dark:text-gray-50">
