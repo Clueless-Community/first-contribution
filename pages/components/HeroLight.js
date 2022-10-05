bad1e837d6985d76a186ee5fed9d1e889c1b814f
codeswithroh <codeswithroh@gmail.com>
2022-10-05 16:46:03 +0530

    added toggling nav bar

--- pages/components/HeroLight.js
+++ pages/components/HeroLight.js
@@ -94,7 +94,7 @@ const HeroDark = () => {
         <div className="w-full justify-center flex">
           <Link href="/whattodo" passHref>
             <button className="px-4 py-2 bg-blue-600 opacity-90 rounded-xl font-bold text-white text-xl my-10 mx-5 hover:bg-blue-500">
-              What to Do? hey
+              What to Do?
             </button>
           </Link>
           <a
@@ -103,7 +103,7 @@ const HeroDark = () => {
             rel="noreferrer"
           >
             <button className="px-4 py-2 border-[#0061ff] border-4 text-[#0061ff] rounded-xl font-bold text-xl my-10 mx-5 hover:bg-blue-500 hover:text-white transition-all hover:border-blue-500">
-              Contribute Now HEY
+              Contribute Now
             </button>
           </a>
         </div>
