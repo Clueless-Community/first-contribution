40cfe584e9e6c4c733bdbbc7fcdfef6d7069f437
Swapnil Aichbhaumik <swapnil.aichbhaumik.cse25@heritageit.edu.in>
2022-10-07 21:04:52 +0530

    Update contributors.js

--- data/contributors.js
+++ data/contributors.js
@@ -1626,13 +1626,13 @@ const contributors = [
     gender: "Male",
    },
   {
-        name : "Swapnil Aichbhaumik",
-        branch : "CSE",
-        college : "HITK",
-        year : 2025, 
-        linkedin : "https://www.linkedin.com/in/swapnil-aichbhaumik-28a980224/",
-        github : "https://github.com/Swap-nil-2003",
-        gender : "Male",
+    name : "Swapnil Aichbhaumik",
+    branch : "CSE",
+    college : "HITK",
+    year : 2025, 
+    linkedin : "https://www.linkedin.com/in/swapnil-aichbhaumik-28a980224/",
+    github : "https://github.com/Swap-nil-2003",
+    gender : "Male",
     },
 ];
 
