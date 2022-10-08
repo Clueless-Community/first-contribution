fa6f2e0328c87e8cacdd60e90310ac2cc85c8787
1Fetcher <sudhirkumar.verma108@gmail.com>
2022-10-08 12:39:33 +0530

    Added my data

--- data/contributors.js
+++ data/contributors.js
@@ -1,5 +1,14 @@
 const contributors = [
     {
+      name : "Sudhir Verma",
+      branch : "A Level (Software)",
+      college : "NIELIT(DOEACC)",
+      year : 2019, //Year of Pass Out
+      linkedin : "https://www.linkedin.com/in/sudhir-kumar-verma-46a661108",
+      github : "https://github.com/1Fetcher",
+      gender : "Male"
+  },
+    {
       name : "Tanmoy Karmakar",
       branch : "Computer Science Engineering",
       college : "University of Engineering and Management",
