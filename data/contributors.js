d9e17e5139a7ffd1c1784942337502bcb0713179
Debmalya <debmalyasen37@gmail.com>
2023-04-27 16:31:35 +0530

    solve the typo mistake and added my data

--- data/contributors.js
+++ data/contributors.js
@@ -2661,16 +2661,16 @@ contributors.unshift(
     linkedin: "https://www.linkedin.com/in/debajyoti-saha-37bb78219/",
     github: "https://github.com/Debajyoti14",
     gender: "Male",
-  }
+  },
   {
-    name : "Debmalya Sen",
-    branch : "Commerce",
-    college : "about to go to college",
-    year : 2027, //Year of Pass Out
-    linkedin : "https://www.linkedin.com/in/debmalyasen1/",
-    github : "https://github.com/debmalya37",
-    gender : "male",
-},
+    name: "Debmalya Sen",
+    branch: "Commerce",
+    college: "about to go to college",
+    year: 2027, //Year of Pass Out
+    linkedin: "https://www.linkedin.com/in/debmalyasen1/",
+    github: "https://github.com/debmalya37",
+    gender: "male",
+  }
 );
 
 export default contributors;
