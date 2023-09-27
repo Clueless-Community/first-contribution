da4d12fd986f0e1fa77f8e05511756952b913eec
7IronSnow7 <divan.debruin25@gmail.com>
2023-09-27 09:13:46 -0400

    Added an algorithm for reversing an integer

---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ReverseInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input any integer here
+            var x = 120;
+
+            Console.WriteLine(ReverseInt(x));
+        }
+        public static int ReverseInt(int x)
+        {
+            var reversedX = 0;
+            while (x != 0)
+            {
+                var pop = x % 10;
+                x /= 10;
+
+                reversedX = reversedX * 10 + pop;
+            }
+            return reversedX;
+        }
+    }
+}
